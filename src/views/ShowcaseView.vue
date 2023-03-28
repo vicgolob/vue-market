44339511d55632d51186588466ba0b2f1a2caae9
francovanney <franco.vanney@incluit.com>
2023-03-28 09:50:08 -0300

    API Call

--- src/views/ShowcaseView.vue
+++ src/views/ShowcaseView.vue
@@ -1,7 +1,32 @@
-<script setup></script>
+<script>
+import { ref } from 'vue'
+
+export default {
+  name: 'ProductsList',
+  setup() {
+    const products = ref([])
+
+    fetch('https://fakestoreapi.com/products?limit=4')
+      .then((response) => response.json())
+      .then((data) => {
+        products.value = data
+      })
+
+    return { products }
+  }
+}
+</script>
 
 <template>
-  <h1>Showcase Page</h1>
+  <div>
+    <h1>Productos</h1>
+    <ul>
+      <li v-for="product in products" :key="product.id" style="color: white">
+        <p>{{ product.title }}</p>
+        <p>{{ product.price }}</p>
+        <p>{{ product.description }}</p>
+        <img :src="product.image" width="20" />
+      </li>
+    </ul>
+  </div>
 </template>
-
-<style></style>
